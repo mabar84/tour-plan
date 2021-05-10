2382858befcc78849ea63950d1f0b735cce79bb4
Baranouski <mabar@tut.by>
2021-05-10 18:01:18 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+});
