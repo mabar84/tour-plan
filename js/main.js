b4f04385bed34d4468f2f8d24af974b1f44cdd75
Baranouski <mabar@tut.by>
2021-05-20 00:06:36 +0300

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -24,3 +24,11 @@ const reviewsSlider = new Swiper(".reviews-slider", {
 });
 
 $(".parallax-window").parallax({});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
